2905dd94bf2ce2189fa7e4c09dd79c41ffaebf69
irie <irie@ayumu-e.jp>
2024-07-12 11:27:20 +0900

    20240712 入江 スケジュールの曜日修正

--- tc_app/src/screens/Schedule.jsx
+++ tc_app/src/screens/Schedule.jsx
@@ -43,8 +43,8 @@ import Popover, { Rect } from 'react-native-popover-view';
 LocaleConfig.locales.jp = {
   monthNames: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月'],
   monthNamesShort: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月'],
-  dayNames: ['月曜日', '火曜日', '水曜日', '木曜日', '金曜日', '土曜日', '日曜日'],
-  dayNamesShort: ['月', '火', '水', '木', '金', '土', '日'],
+  dayNames: ['日曜日', '月曜日', '火曜日', '水曜日', '木曜日', '金曜日', '土曜日'],
+  dayNamesShort: ['日', '月', '火', '水', '木', '金', '土'],
 };
 LocaleConfig.defaultLocale = 'jp';
 
@@ -1217,10 +1217,10 @@ export default function Schedule(props) {
           alignItems: 'center',
           height:40
         },
-        dayTextAtIndex5: {
+        dayTextAtIndex6: {
           color: 'blue'
         },
-        dayTextAtIndex6: {
+        dayTextAtIndex0: {
           color: 'red'
         },
         week: {
